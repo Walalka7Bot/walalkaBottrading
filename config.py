bf8dabcd802185bf192ae2585ead231211b93690
Walalka7Bot <husseinmohamedm88@gmail.com>
2025-07-05 16:40:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-BOT_TOKEN = "7646107517:AAG19U9yHKE1St1bTC86AbFweslqZOack6c"  # ama .env ka akhri
-CHAT_ID = "6846894934"  # beddelkan midka saxda ah (Telegram user ID ama group/channel ID)
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
