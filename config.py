faf6c527c632ef10d05b368fe6bfbac3b3905d7f
Walalka7Bot <husseinmohamedm88@gmail.com>
2025-07-05 15:27:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = int(os.getenv("CHAT_ID"))
+BOT_TOKEN = os.getenv("7646107517:AAG19U9yHKE1St1bTC86AbFweslqZOack6c")
+CHAT_ID = int(os.getenv("6846894934"))
